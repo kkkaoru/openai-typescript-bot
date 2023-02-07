fe13c5fd835b7563b6842708d67435736d05a174
kaoru.ohtaka <ockacru+git@gmail.com>
2023-02-08 07:36:38 +0900

    remove max tokens

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,4 +4,4 @@ import {createExpressReceiver} from "./slack/app";
 
 const config = functions.config();
 const slackHandler = createExpressReceiver(config.slack.secret, config.slack.token, config.openai.api_key);
-export const slack = functions.https.onRequest(slackHandler.app);
+export const slack = functions.region("asia-northeast1").https.onRequest(slackHandler.app);
--- functions/src/utils/artificial-intelligence/open-ai.ts
+++ functions/src/utils/artificial-intelligence/open-ai.ts
@@ -3,11 +3,10 @@ import type {CreateCompletionResponse} from "openai";
 type TextDavinchArgs = {
   prompt: string;
   apiKey: string;
-  maxTokens?: number;
   temperature?: number;
 }
 
-export async function fetchTextDavinci003({prompt, apiKey, maxTokens = 4096, temperature = 0.8}: TextDavinchArgs): Promise<CreateCompletionResponse> {
+export async function fetchTextDavinci003({prompt, apiKey, temperature = 0.6}: TextDavinchArgs): Promise<CreateCompletionResponse> {
   const configuration = new Configuration({
     apiKey,
   });
@@ -15,8 +14,7 @@ export async function fetchTextDavinci003({prompt, apiKey, maxTokens = 4096, tem
   const response = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: prompt || "Please tell me about ChatGPT.",
-    max_tokens: maxTokens,
-    temperature: temperature,
+    temperature,
   });
   return response.data;
 }
