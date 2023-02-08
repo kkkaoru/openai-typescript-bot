2afb887109b08c09c1787ca17519392a1159e61d
kaoru.ohtaka <ockacru+git@gmail.com>
2023-02-08 10:09:03 +0900

    Use thread

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# firebase-hello-world-ts
+# slack bot
+
+
+## Needs
+
+```bash
+# Install firebase tools
+$ npm i -g firebase-tools
+# Install pnpm (or using corepack)
+$ npm i -g pnpm
+```
+
+## Runtime
+
+Deployment on node 16 because of an error when fetching to open ai when running on node 18.
+
--- functions/src/slack/events/mention.ts
+++ functions/src/slack/events/mention.ts
@@ -1,5 +1,5 @@
 import type {App} from "@slack/bolt";
-import {trimMentions, appLog, generateCacheKey, searchCache, fetchTextDavinci003, saveCache, errorLog} from "../../utils";
+import {trimMentions, appLog, searchCache, fetchTextDavinci003, saveCache, errorLog} from "../../utils";
 
 // Require app_mentions:read
 export function setMentionEvent(app: App, openAiApiKey: string) {
@@ -7,26 +7,25 @@ export function setMentionEvent(app: App, openAiApiKey: string) {
     const trimmedText = trimMentions(event.text);
     try {
       appLog("try fetch openai");
-      const cacheKey = generateCacheKey(trimmedText);
-      const cacheMessage = searchCache(cacheKey)?.message;
+      const cacheMessage = searchCache(event.ts)?.message;
       const fetchedMessage = cacheMessage ?? (await fetchTextDavinci003({prompt: trimmedText, apiKey: openAiApiKey}).then((response) => {
         appLog(response);
         appLog("finished fetch openai");
         return response.choices[0].text;
       }));
       if (cacheMessage !== undefined) {
-        appLog(`cache hit ${cacheKey}`);
+        appLog(`cache hit ${event.ts}`);
       }
-      saveCache(cacheKey, {message: fetchedMessage, said: false});
+      saveCache(event.ts, {message: fetchedMessage, said: false});
       appLog(fetchedMessage);
       appLog("try say slack");
-      const cacheSaid = searchCache(cacheKey)?.said;
+      const cacheSaid = searchCache(event.ts)?.said;
       if (cacheSaid === true) {
-        appLog(`said ${cacheKey}`);
+        appLog(`said ${event.ts}`);
         return;
       }
-      await say(`<@${event.user}> ${fetchedMessage}`);
-      saveCache(cacheKey, {message: fetchedMessage, said: false});
+      await say({thread_ts: event.ts, text: `<@${event.user}> ${fetchedMessage}`});
+      saveCache(event.ts, {message: fetchedMessage, said: false});
       appLog("said slack");
     } catch (error) {
       errorLog(error);
