1b1ccba4683103080a2f3ab874c059d1bae4b385
kaoru.ohtaka <ockacru+git@gmail.com>
2023-03-09 21:57:46 +0900

    fix bugs when deploy

--- apps/functions/src/utils/firebase/config/firebase-config.ts
+++ apps/functions/src/utils/firebase/config/firebase-config.ts
@@ -1,6 +1,21 @@
 import { config } from 'firebase-functions';
-import { CreateExpressReceiverArgs } from '@kkkaoru/slack-utils';
+import { CreateExpressReceiverArgs, OpenaiParameters, SlackParameters } from '@kkkaoru/slack-utils';
+
+type AppConfig = {
+  slack: { signing_secret: string } & Omit<SlackParameters, 'signingSecret'>;
+  openai: { api_key: string } & Omit<OpenaiParameters, 'apiKey'>;
+};
 
 export function generateConfig(): CreateExpressReceiverArgs {
-  return config() as CreateExpressReceiverArgs;
+  const appConfig = config() as AppConfig;
+  return {
+    slack: {
+      ...appConfig.slack,
+      signingSecret: appConfig.slack.signing_secret,
+    },
+    openai: {
+      ...appConfig.openai,
+      apiKey: appConfig.openai.api_key,
+    },
+  };
 }
