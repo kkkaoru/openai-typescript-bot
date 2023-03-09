2c47f782b04a40437c9fd7a3f071425d9a9104b5
kaoru.ohtaka <ockacru+git@gmail.com>
2023-03-10 02:33:42 +0900

    fix trim mention

---
+++ .changeset/heavy-weeks-bow.md
@@ -0,0 +1,5 @@
+---
+'@kkkaoru/slack-utils': patch
+---
+
+fix trim mention
--- packages/slack-utils/src/slack/utils/converter/convert-event-to-message.ts
+++ packages/slack-utils/src/slack/utils/converter/convert-event-to-message.ts
@@ -1,7 +1,8 @@
 import { AppMentionEvent } from '@slack/bolt';
 import type { Message } from '@slack/web-api/dist/response/ConversationsRepliesResponse';
+import { trimMentions } from '../trim';
 
 export function convertMessageFromMentionEvent(event: AppMentionEvent): Message {
-  const { type, username, ...message } = event;
-  return message;
+  const { type, username, text, ...message } = event;
+  return { ...message, text: trimMentions(text) };
 }
