fae208df5e458f26cae2879cb7e34a7e5554a160
kaoru.ohtaka <ockacru+git@gmail.com>
2023-03-16 02:02:45 +0900

    update slack utils

--- apps/functions/src/utils/firebase/config/firebase-config.ts
+++ apps/functions/src/utils/firebase/config/firebase-config.ts
@@ -1,17 +1,15 @@
 import { config } from 'firebase-functions';
-import { CreateExpressReceiverArgs, OpenaiParameters, SlackParameters } from '@kkkaoru/slack-utils';
+import { CreateExpressReceiverArgs, SlackParameters } from '@kkkaoru/slack-utils';
+import { OpenaiParameters } from '@kkkaoru/openai-utils';
 
 type AppConfig = {
   slack: { signing_secret: string } & Omit<SlackParameters, 'signingSecret'>;
-  openai: { api_key: string; message_max_count?: string; enabled_system_content?: string } & Omit<
-    OpenaiParameters,
-    'apiKey' | 'messagesMaxCount' | 'enabledSystemContent'
-  >;
+  openai: { api_key: string; message_max_count?: string } & Omit<OpenaiParameters, 'apiKey' | 'messagesMaxCount'>;
 };
 
 export function generateConfig(): CreateExpressReceiverArgs {
   const appConfig = config() as AppConfig;
-  const { api_key, message_max_count, enabled_system_content, ...openai } = appConfig.openai;
+  const { api_key, ...openai } = appConfig.openai;
   const { signing_secret, ...slack } = appConfig.slack;
 
   return {
@@ -22,8 +20,6 @@ export function generateConfig(): CreateExpressReceiverArgs {
     openai: {
       ...openai,
       apiKey: api_key,
-      maxMessagesCount: message_max_count,
-      enabledSystemContent: enabled_system_content,
     },
   };
 }
