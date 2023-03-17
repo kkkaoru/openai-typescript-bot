8b561682eb75a9b8fe174de45db521fa738efdd8
kaoru.ohtaka <ockacru+git@gmail.com>
2023-03-18 01:27:36 +0900

    fix bugs when has retry num

---
+++ .changeset/silly-numbers-lie.md
@@ -0,0 +1,5 @@
+---
+'@kkkaoru/slack-utils': patch
+---
+
+fix bugs when retry
--- packages/slack-utils/src/slack/events/mention/mention-middleware.ts
+++ packages/slack-utils/src/slack/events/mention/mention-middleware.ts
@@ -5,16 +5,20 @@ export type GenerateMiddlewareMentionArgs = CustomTextMessage & AppLogger & Open
 
 export function generateMiddlewareMention({ appLog, errorLog, ...args }: GenerateMiddlewareMentionArgs) {
   return async ({ context, event, say, client }: MiddlewareMentionArgs) => {
+    appLog?.('-- catch mention --');
     appLog?.(event);
     appLog?.(context);
     if (context.retryNum !== undefined) {
       // If when retry, not say
       appLog?.('not say');
+      return;
     }
     try {
       appLog?.('try answer');
       await sayAnswer({ ...args, event, client, say, appLog, errorLog });
+      appLog?.('success answer');
     } catch (error) {
+      errorLog?.('failed answer');
       errorLog?.(error);
       await say({ thread_ts: event.ts, text: `${error?.toString()}` });
     }
